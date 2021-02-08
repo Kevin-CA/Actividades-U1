7284e63af0e79dc6aaf945c89a6924ea37434432
rherr <rherr@DESKTOP-A2FLHKB>
2021-02-07 19:15:10 -0600

    Agregamos Clase principal

---
+++ EjemploGit/src/poo/app/MiApp.java
@@ -0,0 +1,17 @@
+package poo.app;
+
+public class MiApp {
+
+	//EL método run gestiona la ejecución del 
+	// programa, invocando el mínimo de sentencias.
+	static void run() {
+		System.out.println("Hola Mundo");
+	}
+	// El método principal debe quedar reducido al
+	// menor número de líneas
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         run();
+	}
+
+}
